d8ecf22a028e07e706a768d20f81b919c6c43e49
Mettralla <67493167+Mettralla@users.noreply.github.com>
2023-09-14 00:08:36 -0300

    Agrego update message

--- app/controllers/message_controller.py
+++ app/controllers/message_controller.py
@@ -10,7 +10,7 @@ class MessageController:
 
         msg = Message.get_message(message_id)
         return jsonify(msg.serialize()), 200
-    
+
     @classmethod
     def get_messages(cls):
         channel_id = request.args.get('channel_id')
@@ -38,5 +38,36 @@ class MessageController:
         if session['user_id'] == msg.user_id:
             Message.delete_message(message_id)
             return jsonify({'message': 'Message deleted successfully'}), 204
+        else:
+            raise ForbiddenAction()
+        
+    @classmethod
+    def create_message(cls):
+        msg_data = request.json
+
+        new_msg = Message.validate_data([
+            msg_data.get('message_body'),
+            session['user_id'],
+            msg_data.get('channel_id') 
+        ])
+        
+        Message.create_message(new_msg)
+        return jsonify({'message': 'Message created successfully'}), 201
+    
+    @classmethod
+    def update_message(cls, message_id):
+        if not Message.exist(message_id):
+            raise NotFound(message_id, "message")
+        
+        og_msg = Message.get_message(message_id)
+        
+        if session['user_id'] == og_msg.user_id:
+            update_data = request.json
+            
+            Message.update_message((
+                update_data.get('message_body', og_msg.message_body),
+                message_id
+            ))
+            return jsonify({'message': 'Message updated successfully'}), 200
         else:
             raise ForbiddenAction()
--- app/models/user_model.py
+++ app/models/user_model.py
@@ -67,11 +67,11 @@ class User:
     def validate_data(cls, data) -> 'User':
         """Validate user data"""
         new_username = data.get('username')
-        if len(new_username) < 3 and not isinstance(new_username, str):
+        if len(new_username) < 3:
             raise InvalidDataError("Username must have at least three characters")
 
         new_password = data.get('password') 
-        if len(new_password) < 8 and not isinstance(new_password, str):
+        if len(new_password) < 8:
             raise InvalidDataError("Password must have at least eight characters")
 
         return User(username = new_username, password = new_password)
