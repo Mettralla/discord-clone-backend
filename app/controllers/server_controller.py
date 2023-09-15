fd0cc809125771ef9f698beb9d916eec7f6c82ee
Ferco7 <bynanofer@hotmail.com>
2023-09-15 14:25:34 -0300

    revision de auth

--- app/controllers/server_controller.py
+++ app/controllers/server_controller.py
@@ -1,26 +1,28 @@
 """Server Controller"""
 
-from flask import request, jsonify
+from flask import request, jsonify, session
 from ..models.server_model import Server
-from ..models.exceptions import NotFound
+from ..controllers.auth_controller import AuthController
+from ..models.exceptions import InvalidDataError
 
 
 class ServerController:
     """Class Server Controller"""
 
     @classmethod
+    @AuthController.login_required
     def create_server(cls):
         """Create Server"""
         server_data = request.json
-        server = Server(
-            server_name=server_data.get("server_name"),
-            server_description=server_data.get("server_description"),
-            owner_id=server_data.get("owner_id"),
-        )
-        Server.create_server(server)
-        return jsonify({"message": "Server created successfully"}), 201
+        try:
+            server = Server.validate_data(server_data)
+            Server.create_server(server)
+            return jsonify({"message": "Server created successfully"}), 201
+        except InvalidDataError as e:  # pylint: disable = invalid-name
+            return jsonify({"error": str(e)}), 400
 
     @classmethod
+    @AuthController.login_required
     def get_servers(cls):
         """Get Servers"""
         server_name = request.args.get("server_name")
@@ -45,43 +47,49 @@ class ServerController:
         return jsonify(response), 200
 
     @classmethod
+    @AuthController.login_required
     def get_server(cls, server_id):
         """Get Server"""
-        server = Server.get_server(server_id)
-        if server:
-            server_dict = {
-                "server_id": server.server_id,
-                "server_name": server.server_name,
-                "server_description": server.server_description,
-                "owner_id": server.owner_id,
-            }
-            return jsonify(server_dict), 200
-
-        raise NotFound(server_id, "server")
+        if Server.exist(server_id):
+            server = Server.get_server(server_id)
+            if server:
+                server_dict = {
+                    "server_id": server.server_id,
+                    "server_name": server.server_name,
+                    "server_description": server.server_description,
+                    "owner_id": server.owner_id,
+                }
+                return jsonify(server_dict), 200
+        return jsonify({"error": "Server not found"}), 404
 
     @classmethod
+    @AuthController.login_required
     def update_server(cls, server_id):
         """Update Server"""
         update_data = request.json
-        updated_fields = {}
+        user_id = session.get("user_id")
 
-        if "server_name" in update_data:
-            updated_fields["server_name"] = update_data["server_name"]
+        if Server.exist(server_id, user_id):
+            updated_fields = {}
+            if "server_name" in update_data:
+                updated_fields["server_name"] = update_data["server_name"]
 
-        if "server_description" in update_data:
-            updated_fields["server_description"] = update_data["server_description"]
+            if "server_description" in update_data:
+                updated_fields["server_description"] = update_data["server_description"]
 
-        if updated_fields:
-            Server.update_server(server_id, updated_fields)
-            return jsonify({"message": "Server updated successfully"}), 200
+            if updated_fields:
+                Server.update_server(server_id, updated_fields)
+                return jsonify({"message": "Server updated successfully"}), 200
 
-        return jsonify({"message": "No valid fields to update"}), 400
+            return jsonify({"message": "No valid fields to update"}), 400
+        else:
+            return jsonify({"error": "Server not found or you are not the owner"}), 404
 
     @classmethod
+    @AuthController.login_required
     def delete_server(cls, server_id):
         """Delete Server"""
-        if Server.get_server(server_id):
+        if Server.exist(server_id):
             Server.delete_server(server_id)
             return jsonify({"message": "Server deleted successfully"}), 204
-
-        raise NotFound(server_id, "server")
+        return jsonify({"error": "Server not found"}), 404
